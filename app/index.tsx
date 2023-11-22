84bc76fa6f0169d9ee1e1adf48a1d6bc83ca8426
Miguel Quispe <dev@miguelquispe.com>
2023-11-22 14:44:24 -0500

    feat: organization bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,41 @@
 import React, { useState } from "react";
 import { Link } from "expo-router";
-import { Button, Heading, Text, YStack } from "tamagui";
+import { Button, Heading, Stack, Text, XStack, YStack } from "tamagui";
 // import MapView, { Marker } from "react-native-maps";
 import { color } from "@tamagui/themes";
 import { ButtonApp } from "../components/Button";
 import { SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Home() {
   const [selectedMarket, setSelectedMarket] = useState(null);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <YStack p="$6" justifyContent="center">
-        <YStack gap={"$2"} mb="$10">
-          <Heading textAlign="center">LOCAL FOOD</Heading>
-          <Heading textAlign="center">SHARE HUB</Heading>
+      <YStack f={1} p={"$3"}>
+        <XStack p="$4" borderRadius={20} bg={"white"}>
+          <XStack gap="$4" f={1}>
+            <FontAwesome name="user" size={20} />
+            <Text>Little Stars Refuge</Text>
+          </XStack>
+          <FontAwesome name="bars" size={20} />
+        </XStack>
+        <YStack p="$6" justifyContent="center" f={1}>
+          <YStack gap={"$2"} mb="$10">
+            <Heading textAlign="center">LOCAL FOOD</Heading>
+            <Heading textAlign="center">SHARE HUB</Heading>
+          </YStack>
+          <Link href={"/map"} asChild>
+            <ButtonApp
+              bg={"$green10Light"}
+              pressStyle={{
+                bg: "$green10Dark",
+              }}
+            >
+              <Text color={"white"}>Find Markets</Text>
+            </ButtonApp>
+          </Link>
         </YStack>
-        <Link href={"/map"} asChild>
-          <ButtonApp
-            bg={"$green10Light"}
-            pressStyle={{
-              bg: "$green10Dark",
-            }}
-          >
-            <Text color={"white"}>Find Markets</Text>
-          </ButtonApp>
-        </Link>
       </YStack>
     </SafeAreaView>
   );
